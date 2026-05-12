da221741b4cde39d42c24b5cbd20c4f2959a9ddd
PavanSaiNekkala <91945047+PavanSaiNekkala@users.noreply.github.com>
2026-05-12 17:31:24 +0530

    Update balance_extractor.py

--- core/balance_extractor.py
+++ core/balance_extractor.py
@@ -1,1 +1,39 @@
+import yfinance as yf
+import numpy as np
 
+def extract_balance_sheet(symbol):
+
+    ticker = yf.Ticker(symbol)
+
+    balance = ticker.balance_sheet
+
+    data = {
+
+        "Total Assets": np.nan,
+        "Total Debt": np.nan,
+        "Cash": np.nan,
+        "Equity": np.nan
+
+    }
+
+    try:
+        data["Total Assets"] = balance.loc["Total Assets"].iloc[0]
+    except:
+        pass
+
+    try:
+        data["Total Debt"] = balance.loc["Total Debt"].iloc[0]
+    except:
+        pass
+
+    try:
+        data["Cash"] = balance.loc["Cash And Cash Equivalents"].iloc[0]
+    except:
+        pass
+
+    try:
+        data["Equity"] = balance.loc["Stockholders Equity"].iloc[0]
+    except:
+        pass
+
+    return data
