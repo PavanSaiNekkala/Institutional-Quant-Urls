87b16a780e78b757943ef5787d0b85c04409bb9a
PavanSaiNekkala <91945047+PavanSaiNekkala@users.noreply.github.com>
2026-05-12 17:30:48 +0530

    Update market_extractor.py

--- core/market_extractor.py
+++ core/market_extractor.py
@@ -1,1 +1,36 @@
+import yfinance as yf
+import numpy as np
 
+def extract_market_data(symbol):
+
+    try:
+
+        ticker = yf.Ticker(symbol)
+
+        fast = ticker.fast_info
+
+        return {
+
+            "Current Price": fast.get("lastPrice", np.nan),
+
+            "Market Cap": fast.get("marketCap", np.nan),
+
+            "Day High": fast.get("dayHigh", np.nan),
+
+            "Day Low": fast.get("dayLow", np.nan),
+
+            "52W High": fast.get("yearHigh", np.nan),
+
+            "52W Low": fast.get("yearLow", np.nan),
+
+            "Volume": fast.get("lastVolume", np.nan),
+
+            "Avg Volume": fast.get("tenDayAverageVolume", np.nan)
+
+        }
+
+    except Exception as e:
+
+        return {
+            "Market Error": str(e)
+        }
