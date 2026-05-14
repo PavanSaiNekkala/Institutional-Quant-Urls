e931beace05509bcf85d26ff1626f2ad60ce0835
PavanSaiNekkala <91945047+PavanSaiNekkala@users.noreply.github.com>
2026-05-14 12:39:55 +0530

    Update db_manager.py

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -3,12 +3,11 @@
 # =========================================================
 
 import duckdb
-import pandas as pd
 
 from pathlib import Path
 
 # =========================================================
-# PATHS
+# BASE PATH
 # =========================================================
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,31 +17,13 @@ DATABASE_DIR = (
     / "database"
 )
 
-OUTPUT_DIR = (
-    BASE_DIR
-    / "output"
-)
-
-DB_FILE = (
-    DATABASE_DIR
-    / "institutional_quant.db"
-)
-
-CSV_FILE = (
-    OUTPUT_DIR
-    / "enriched_stock_data.csv"
-)
-
-# =========================================================
-# CREATE DIRECTORIES
-# =========================================================
-
 DATABASE_DIR.mkdir(
     exist_ok=True
 )
 
-OUTPUT_DIR.mkdir(
-    exist_ok=True
+DATABASE_FILE = (
+    DATABASE_DIR
+    / "institutional_quant.db"
 )
 
 # =========================================================
@@ -52,77 +33,8 @@ OUTPUT_DIR.mkdir(
 def get_connection():
 
     conn = duckdb.connect(
-        str(DB_FILE)
+
+        str(DATABASE_FILE)
     )
 
-    # =============================================
-    # AUTO REBUILD DATABASE
-    # =============================================
-
-    try:
-
-        tables = conn.execute(
-            "SHOW TABLES"
-        ).fetchall()
-
-        table_names = [
-
-            table[0]
-            for table in tables
-        ]
-
-        # =========================================
-        # REBUILD IF TABLE MISSING
-        # =========================================
-
-        if "enriched_stocks" not in table_names:
-
-            rebuild_database(conn)
-
-    except Exception:
-
-        rebuild_database(conn)
-
     return conn
-
-# =========================================================
-# REBUILD DATABASE
-# =========================================================
-
-def rebuild_database(conn):
-
-    print("=" * 60)
-    print("REBUILDING DATABASE...")
-    print("=" * 60)
-
-    if CSV_FILE.exists():
-
-        df = pd.read_csv(
-            CSV_FILE
-        )
-
-        conn.register(
-            "temp_df",
-            df
-        )
-
-        conn.execute(
-            """
-            CREATE OR REPLACE TABLE
-            enriched_stocks AS
-
-            SELECT *
-            FROM temp_df
-            """
-        )
-
-        print(
-            "Database rebuilt successfully."
-        )
-
-    else:
-
-        print(
-            "CSV file missing. "
-            "Database rebuild failed."
-        )
